3ef7e9d1d29b4726c884469d64e251689ed90014
vedant kulkarni <vedantkulkarni1155@gmail.com>
2025-07-01 12:32:42 +0530

    Initial setup: React + Vite + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../src/App.css";
+const App = () => {
+  return (
+    <div className="min-h-screen bg-blue-200 flex items-center justify-center">
+      <h1 className="text-3xl font-bold underline text-blue-800 p-6 bg-yellow-300 rounded-lg">
+        Hello React + Tailwind!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
